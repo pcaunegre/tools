ea2b7ed947e6a3f1ce969484e8e73aa05469e63a
Pascal <pascal.caunegre@free.fr>
2024-05-02 11:50:55 +0200

    some more formatting

--- extractCFD.py
+++ extractCFD.py
@@ -6,6 +6,9 @@
 #   and correlate with weather conditions
 #   report in a csv/html/pdf file
 # 
+# 
+# Author: Pascal Caunegre
+# Licence: CC-BY-NC-SA
 # ------------------------------------------------------------------------------
 
 # For PDF file generation, it may be needed to install xhtml2pdf lib:
@@ -58,7 +61,12 @@ DECONAMES = [0, 'ValLouron', 'St Hilaire', 'St Andre', 'Chalvet W', 'Bleine', 'S
 SOURCE_URL="https://parapente.ffvl.fr/cfd/liste/saison/"
 WTHRMAP_URL="https://www.wetterzentrale.de/reanalysis.php?"
 
-
+#https://meteocentre.com/radiosonde/get_sounding.php?lang=fr&show=1&hist=1&yyyy=2024&mm=04&dd=27&area=qc&stn=07510&run=12
+# https://meteocentre.com/radiosonde/get_sounding.php?lang=fr&yyyy=2024&mm=04&dd=27&area=qc&stn=07510&run=12
+# stn=0714 : Trappes
+# stn=07510 : Bordeaux
+SOUNDMAP_URL="https://meteocentre.com/radiosonde/get_sounding.php?lang=fr&show=1&hist=1"
+SOUNDINGSTATION = '07510'
 
 # ------------------------------------------------------------------------------
 # help
@@ -67,21 +75,22 @@ def userHelp():
     print("%s : parse CFD data base to extract days of flight" % sys.argv[0] )
     print("%s -deco <deco nbr> -an <saeson> [-minkm <min flight dist>] [-minfl <n>]\n\
                    [-out <file>] [-csv] [-html] [-pdf] [ -h ]" % sys.argv[0] )
+    print("\nArguments:\n" )
     print("  -deco 1: ValLouron 2: St Hilaire 3: St Andre 4: Chalvet  5: Bleine   6: Serpaton" )
-    print("        7: Pouncho   8: Greoliere  9: Arbas   10: Forclaz 11: Planfait" )
-    print("  OR -decoId <deco Id>" )
-    print("  -an <saeson> " )
+    print("        7: Pouncho   8: Greoliere  9: Arbas   10: Forclaz 11: Planfait\n" )
+    print("     OR\n  -decoId <deco Id>  (as stated in FFVL database)\n" )
+    print("  -an <year>     : year to explore" )
     print("  -minkm <km>    : filter flights by distance" )
     print("  -minfl <n>     : min nbr of flights to consider the day as a good day" )
-    print("  -out <outfile> : output file (csv format) " )
+    print("  -out <outfile> : output file name/path" )
     print("  -csv           : generate a csv output file " )
     print("  -html          : generate an html output file " )
     print("  -pdf           : generate a pdf output file " )
-    print("  -h     : help" )
+    print("  -h             : help" )
     print("" )
     print("Example:" )
-    print("  I want to filter days with more that 5 flights of 40km at Forclaz" )
-    print("%s -deco 10 -an 2022 -minkm 40 -minfl 5 -out Forclaz2022.csv -csv" % sys.argv[0] )
+    print("  I want to filter days with more that 5 flights of 40km at Forclaz for 2022" )
+    print("%s -deco 10 -an 2022 -minkm 40 -minfl 5 -out Forclaz2022 -csv\n" % sys.argv[0] )
     exit(0)
 
 # ------------------------------------------------------------------------------
@@ -109,7 +118,7 @@ def readArgs():
             season = int(sys.argv[i+1])
             i=i+1
         elif sys.argv[i]=="-minkm": 
-            minkm = int(sys.argv[i+1])
+            minkm = sys.argv[i+1]
             i=i+1
         elif sys.argv[i]=="-minfl": 
             minfl = int(sys.argv[i+1])
@@ -171,7 +180,7 @@ def getAndSortData(year):
         # print(elemDict.keys())
         date = elemDict['date']
         if re.match(pattern,date):
-            if (float(elemDict['distance']) >= minkm):
+            if (float(elemDict['distance']) >= int(minkm)):
                 if date in dateTab:
                     dateTab[date] = dateTab[date] + 1
                 else:
@@ -189,26 +198,42 @@ def csvOutput():
     
     # forge output
     # -----------------------------
-    headerlist = [ deconame, '', '' ]
+    headerlist = [ deconame, str(season), '' ]
     outStr = ",".join(headerlist) + '\n'
-    headerlist = [ 'Date', 'NbVols' , 'Carte'  ]
+    headerlist = [ 'Date', 'NbVols > '+minkm+'km' , 'Cartes Meteo'  ]
     outStr += ",".join(headerlist) + '\n'
 
     for d in sorted(dateTab.keys()):
         if (dateTab[d] >= minfl):
             spl_date = d.split('-')
-            wthrmap = WTHRMAP_URL + 'jaar=' + spl_date[0] +'&maand=' + spl_date[1] + '&dag=' + spl_date[2] + '&uur=000&var=1&map=1&model=avn'
-            lineList = [ d, str(dateTab[d]), wthrmap ]
+            wthrmap1 = forgeLink(spl_date,'1')
+            wthrmap2 = forgeLink(spl_date,'2')
+            soundmap = forgeSdLink(spl_date,SOUNDINGSTATION)
+            lineList = [ d, str(dateTab[d]), wthrmap1, wthrmap2, soundmap ]
             outStr += ",".join(lineList) + '\n'
     
     # output in csv file
     # -----------------------------
-    f = open(outfile, 'w', newline='')
+    f = open(outfile+'.csv', 'w', newline='')
     print(outStr, file=f)
     f.close 
     print("CSV file generated") 
 
     return(outStr)
+
+# ------------------------------------------------------------------------------
+# utilities to forge links
+# ------------------------------------------------------------------------------
+def forgeLink(spl_date,var):
+
+    ret = WTHRMAP_URL + 'jaar=' + spl_date[0] +'&maand=' + spl_date[1] + '&dag=' + spl_date[2] + '&uur=000&var='+var+'&map=1&model=avn'
+    return(ret)
+    
+def forgeSdLink(spl_date,station):
+
+    ret = SOUNDMAP_URL + '?lang=fr&yyyy=' + spl_date[0] + '&mm=' + spl_date[1] + '&dd=' + spl_date[2] + '&area=qc&stn=' + station + '&run=12'
+    return(ret)
+
 
 # ------------------------------------------------------------------------------
 # generate a html formatted ouput
@@ -219,23 +244,24 @@ def htmlOutput():
 
     global dateTab
     outStr = '<table border="1" class="dataframe">\n'
-    outStr += '<thead><tr><th>' + deconame + '</th></tr></thead>\n'
+    outStr += '<thead><tr><th>' + deconame + '</th><th>' + str(season) + '</th></tr></thead>\n'
     outStr += '<tbody>\n'
-    outStr += '<tr><th>Date</th><th>NbVols</th><th>Cartes</th><th>Vols</th></tr>\n'
+    outStr += '<tr><th>Date</th><th>NbVols > ' + minkm + 'km</th><th>Cartes Meteo</th><th>Vols</th></tr>\n'
     FL_URL="https://parapente.ffvl.fr/cfd/liste/saison/"
     
     for d in sorted(dateTab.keys()):
         if (dateTab[d] >= minfl):
             spl_date = d.split('-')
-            wthrmap1 = WTHRMAP_URL + 'jaar=' + spl_date[0] +'&maand=' + spl_date[1] + '&dag=' + spl_date[2] + '&uur=000&var=1&map=1&model=avn'
-            wthrmap2 = WTHRMAP_URL + 'jaar=' + spl_date[0] +'&maand=' + spl_date[1] + '&dag=' + spl_date[2] + '&uur=000&var=2&map=1&model=avn'
+            wthrmap1 = forgeLink(spl_date,'1')
+            wthrmap2 = forgeLink(spl_date,'2')
+            soundmap = forgeSdLink(spl_date,SOUNDINGSTATION)
             yr = season
             if (int(spl_date[1]) < 8):
                 yr -= 1
-            fl_link = FL_URL + str(yr) + '/deco/' + str(deco) + '/date/' + d
+            fl_link = FL_URL + str(yr) + '/deco/' + str(deco) + '/date/' + d + '?sort=desc&order=km'
             
             outStr += '<tr><th>'+str(d)+'</th><th>'+str(dateTab[d])+'</th><th><a href="'+\
-                wthrmap1+'">Pression  </a><a href="'+wthrmap2+'">  Z850</a></th><th><a href="'+fl_link+'">vols</a></th></tr>\n'
+                wthrmap1+'">Pression</a>  |  <a href="'+wthrmap2+'">Z850</a>   |  <a href="'+soundmap+'">Sounding</a> </th><th><a href="'+fl_link+'">Traces CFD</a></th></tr>\n'
     
     outStr += '</tbody>\n'
     outStr += '</table>\n'
